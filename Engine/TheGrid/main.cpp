23913e2008d8296addd1c7867f72577f3d8ff28e
adrian.skupien <adrian.skupien@bestgliwice.pl>
2011-05-29 15:46:17 +0000

    Szkielet głównego podsystemu : )

--- Engine/TheGrid/main.cpp
+++ Engine/TheGrid/main.cpp
@@ -1,8 +1,22 @@
+#include "cGameMgr.h"
 #include <iostream>
 
 
 int main()
 {
+	// ...
+	cGameMgr *tmp = cGameMgr::get();
+
+	tmp->initGraphics();
+	tmp->initGameSystems();
+
+	// game : D
+	while ( tmp->RunGameLoop() ) ;
+
+	tmp->deinitGameSystems();
+	tmp->deinitGraphics();
+
+
 	using namespace std;
 	cout << "Jestem zajebisty : D "<<endl;
 	system("pause");
