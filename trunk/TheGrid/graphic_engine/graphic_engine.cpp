2ffa99de886f2368c8d9c4722697aaab4a002fa3
getoxs@gmail.com <getoxs@gmail.com@aff433ed-6625-f04a-8c50-0217fc81815f>
2011-09-28 06:11:52 +0000

    Super duper zmiany!

--- trunk/TheGrid/graphic_engine/graphic_engine.cpp
+++ trunk/TheGrid/graphic_engine/graphic_engine.cpp
@@ -4,16 +4,17 @@
 
 extern int framesPerSecond;
 GraphicEng* GraphicEng::instance = NULL;
-GLBatch triBach;
-GLfloat blockSize = 0.1f;
-GLfloat vVerts[] = { -blockSize, -blockSize, 0.0f, 
-	                  0.0f, blockSize, 0.0f,
-					  blockSize,  -blockSize, 0.0f};
-GLBatch triBach2;
-GLfloat vVerts2[] = { -blockSize+0.5f, -blockSize+0.5f, 0.5f, 
-	                  0.5f, blockSize+0.5f, 0.5f,
-					  blockSize+0.5f,  -blockSize+0.5f, 0.5f};
-
+//GLBatch *gridBach;
+//GLBatch triBach;
+//GLfloat blockSize = 0.1f;
+//GLfloat vVerts[3][3] = { -blockSize, -blockSize, 0.0f, 
+//	                  0.0f, blockSize, 0.0f,
+//					  blockSize,  -blockSize, 0.0f};
+//GLBatch triBach2;
+//GLfloat vVerts2[] = { -blockSize+0.5f, -blockSize+0.5f, 0.5f, 
+//	                  0.5f, blockSize+0.5f, 0.5f,
+//					  blockSize+0.5f,  -blockSize+0.5f, 0.5f};
+GLMatrixStack		modelViewMatrix;
 
 	//////////////////////////////////////////////////////////////////////
 	// funkcja generuj¹ca scenê 3D
@@ -23,10 +24,19 @@ GLfloat vVerts2[] = { -blockSize+0.5f, -blockSize+0.5f, 0.5f,
     // czyszczenie bufora koloru, bufora g³êbokoœci i bufora szblonowego
     glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT );
 		
-		shaderManager.UseStockShader(GLT_SHADER_IDENTITY, clRed);
-		triBach.Draw();
-		shaderManager.UseStockShader(GLT_SHADER_IDENTITY, clPink);
-		triBach2.Draw();
+		glLineWidth(5);
+		shaderManager.UseStockShader(GLT_SHADER_SHADED, modelViewMatrix.GetMatrix());
+		grid.renderFields();
+		
+    //private constructor
+		for(int i = 0; i<GRID_HEIGHT; i++)
+			for(int j = 0; j<GRID_WIDTH; j++)
+				if(gridObjects[i][j] != 0)
+				{
+					gridObjects[i][j]->batch->Draw();
+				}
+		if(playerBatch != 0 )
+			playerBatch->Draw();
 
 		if(this->showFpsInfo)
 		{
@@ -41,11 +51,10 @@ GLfloat vVerts2[] = { -blockSize+0.5f, -blockSize+0.5f, 0.5f,
 		}
 
 		// skierowanie poleceñ do wykonania
-		//glFlush();
+		glFlush();
 		glutSwapBuffers();
 		glutPostRedisplay();
 	}
-
 	//////////////////////////////////////////////////////////////////////
 	// inicjalizacja sta³ych elementów maszyny stanu OpenGL
 	//////////////////////////////////////////////////////////////////////
@@ -54,20 +63,14 @@ GLfloat vVerts2[] = { -blockSize+0.5f, -blockSize+0.5f, 0.5f,
     // kolor t³a - zawartoœæ bufora koloru
     glClearColor( 1.0f, 1.0f, 1.0f, 1.0f );
 		shaderManager.InitializeStockShaders();
+    glEnable( GL_DEPTH_TEST );
 		
-		// Load up a triangle
-		triBach.Begin(GL_TRIANGLES, 3);
-		triBach.CopyVertexData3f(vVerts);
-		triBach.End();
-		// Load up a triangle
-		triBach2.Begin(GL_TRIANGLES, 3);
-		triBach2.CopyVertexData3f(vVerts2);
-		triBach2.End();
-
+		grid.generateBashGrid(-1, 1, -1, 1, -0.1f,  0.0f);
 		
+		modelViewMatrix.LoadIdentity();
+		viewFrustum.SetOrthographic(-1, 1, -1, 1, -1, 1);
     // w³¹czenie mechanizmów u¿ywanych podczas renderingu tekstu
     //InitDrawText();
-    glEnable( GL_DEPTH_TEST );
 	}
 
 	//////////////////////////////////////////////////////////////////////
@@ -77,6 +80,7 @@ GLfloat vVerts2[] = { -blockSize+0.5f, -blockSize+0.5f, 0.5f,
 	{
 		// obszar renderingu - ca³e okno
 		glViewport( 0, 0, width, height );
+		viewFrustum.SetOrthographic(-1, 1, -1, 1, -1, 1);
 	}
 
 	void GraphicEng::DeleteScene()
