d55c1a9a99a44fb31d75645ce7668bc85ef981f1
getoxs@gmail.com <getoxs@gmail.com@aff433ed-6625-f04a-8c50-0217fc81815f>
2011-09-29 18:48:11 +0000

    dodanie filtrów

--- trunk/TheGrid/graphic_engine/GroundGridLayer.h
+++ trunk/TheGrid/graphic_engine/GroundGridLayer.h
@@ -8,16 +8,19 @@ class GroundGridLayer :
 private:
 	GLBatch *lineBash;
 	GLBatch *groundBatch[GROUND_GRID_H][GROUND_GRID_W];
+	int **stateTable;
 public:
 	Grid* grid;
 
-	GroundGridLayer(Grid* _grid);
+	
+	void rescanState(int **stateTable);
 	void setFieldColor(int x, int y, GLfloat r, GLfloat g, GLfloat b, GLfloat a);
 	inline GLfloat *getPosition(int gridX, int gridY, int offsetX, int offsetY);
 
 	void Init();
 	void Render(M3DMatrix44f modelViewMatrix);
 	void Dispose();
+	GroundGridLayer(Grid* _grid);
 	virtual ~GroundGridLayer(void);
 };
 
